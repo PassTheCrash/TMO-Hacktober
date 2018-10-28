6d8fa91428f14c6297c0d78c50d70246b362aac5
John Martin <me@looklittlejohn.com>
2018-10-28 00:37:44 -0500

    yes

--- routes/api.php
+++ routes/api.php
@@ -29,15 +29,35 @@ Route::post("/session/{sessionId}/transcription", function(Request $req) {
     $pusher->trigger($req->sessionId, 'transcription', $req->getContent());
 
 
-
-
 //    return event(new SessionTranscriptionReceived($req->sessionId, $req->getContent()));
 });
 
 Route::post("/session/{sessionId}/keywords", function(Request $req) {
-    return response();
+    $options = array(
+        'cluster' => 'us2',
+        'useTLS' => true
+    );
+    $pusher = new Pusher\Pusher(
+        '50e58c5d5b112270f798',
+        'e3badd5acd4287cf66dc',
+        '633217',
+        $options
+    );
+
+    $pusher->trigger($req->sessionId, 'keywords', $req->getContent());
 });
 
 Route::post("/session/{sessionId}/sentiment", function(Request $req) {
-    return response();
+    $options = array(
+        'cluster' => 'us2',
+        'useTLS' => true
+    );
+    $pusher = new Pusher\Pusher(
+        '50e58c5d5b112270f798',
+        'e3badd5acd4287cf66dc',
+        '633217',
+        $options
+    );
+
+    $pusher->trigger($req->sessionId, 'sentiment', $req->getContent());
 });
