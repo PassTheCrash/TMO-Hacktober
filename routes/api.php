a392b4418766630154e3b30e0b3a4a78f451e46c
John Martin <me@looklittlejohn.com>
2018-10-28 02:49:45 -0500

    yes

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,33 @@ Route::post("/session/{sessionId}/transcription", function(Request $req) {
 });
 
 Route::post("/session/{sessionId}/keywords", function(Request $req) {
+    $buffer = array();
+    $exceptions = [
+        "t mobile support",
+        "today",
+        "my phone",
+        "my computers",
+        "My name",
+        "my name",
+        "a second"
+    ];
+
+    $keywords = json_decode($req->getContent())->KeyPhrases;
+
+    for($i = 0; $i < count($keywords); $i++) {
+        if(in_array($keywords[$i]->Text, $exceptions)) {
+        }
+
+        else if($keywords[$i]->Score < 0.97) {
+        }
+
+        else {
+            $confidence = $keywords[$i]->Score >= 0.98 ? "HIGH" : "LOW";
+            $buffer[]  = array("text" => $keywords[$i]->Text, "confidence" => $confidence);
+        }
+    }
+
+
     $options = array(
         'cluster' => 'us2',
         'useTLS' => true
@@ -44,7 +71,9 @@ Route::post("/session/{sessionId}/keywords", function(Request $req) {
         $options
     );
 
-    $pusher->trigger($req->sessionId, 'keywords', $req->getContent());
+    $pusher->trigger($req->sessionId, 'keywords', $buffer);
+
+    return response(1);
 });
 
 Route::post("/session/{sessionId}/sentiment", function(Request $req) {
@@ -60,4 +89,8 @@ Route::post("/session/{sessionId}/sentiment", function(Request $req) {
     );
 
     $pusher->trigger($req->sessionId, 'sentiment', $req->getContent());
+});
+
+Route::get("/customer/{customerId}", function(Request $req){
+    return response($req->customerId);
 });
